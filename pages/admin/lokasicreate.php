695657fe45db417e3a39b6200ccbe01f95931029
wangilindalestarii <wangilindalestari23@gmail.com>
2023-05-19 17:16:01 +0800

    Membuat Perintah Create Data

--- pages/admin/lokasicreate.php
+++ pages/admin/lokasicreate.php
@@ -1,12 +1,47 @@
+<?php
+
+if (isset($_POST['button_create'])) {
+
+      $database = new Database();
+      $db = $database->getConnection();
+
+      $validateSQL = "SELECT * FROM lokasi WHERE nama_lokasi = ?";
+      $stmt = $db->prepare($validateSQL);
+      $stmt->bindParam(1, $_POST['nama_lokasi']);
+      $stmt->execute();
+      if ($stmt->rowCount() > 0) {
+?>
+            <div class="alert alert-danger alery-dismissible">
+                  <button type="button" class="close" data-dismiss="alert" aria-hidden="true"></button>
+                  <h5><i class="icon fas fa-ban"></i>Gagal</h5>
+                  Nama lokasi sama sudah ada
+            </div>
+<?php
+      } else {
+            $insertSQL = "INSERT INTO lokasi SET nama_lokasi = ?";
+            $stmt = $db->prepare($insertSQL);
+            $stmt->bindParam(1, $_POST['nama_lokasi']);
+            if ($stmt->execute()) {
+                  $_SESSION['hasil'] = true;
+                  $_SESSION['pesan'] = "Berhasil simpan data";
+            } else {
+                  $_SESSION['hasil'] = false;
+                  $_SESSION['pesan'] = "Gagal simpan data";
+            }
+            echo "<meta http-equiv='refresh' content='0;url=?page=lokasiread'>";
+      }
+}
+?>
+
 <section class="content-header">
-      <div class="container-fluid">
+      <div class="containerfluid">
             <div class="row mb2">
                   <div class="col-sm-6">
                         <h1>Tambah Data Lokasi</h1>
                   </div>
                   <div class="col-sm-6">
                         <ol class="breadcrumb float-sm-right">
-                              <Li class="breadcrumb-item"><a href="?page=home">Home</a></li>
+                              <li class="breadcrumb-item"><a href="?page=home">Home</a></li>
                               <li class="breadcrumb-item"><a href="?page=lokasiread">Lokasi</a></li>
                               <li class="breadcrumb-item active">Tambah Data</li>
                         </ol>
@@ -22,7 +57,7 @@
             <div class="card-body">
                   <form method="POST">
                         <div class="form-group">
-                              <label for="nama_lokasi"> Nama Lokasi </label>
+                              <label for="nama_lokasi">Nama Lokasi</label>
                               <input type="text" class="form-control" name="nama_lokasi">
                         </div>
                         <a href="?page=lokasiread" class="btn btn-danger btn-sm float-right">
@@ -35,4 +70,5 @@
             </div>
       </div>
 </section>
+
 <?php include_once "partials/scripts.php" ?>
